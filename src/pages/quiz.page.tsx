eae24e5acd5df5776ffdf7b12ebb285b758d6bb6
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2025-02-02 18:18:14 +0530

    fix typo

--- src/pages/quiz.page.tsx
+++ src/pages/quiz.page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
-import { ShowResult } from "@/components/custome/showResult";
+import { ShowResult } from "@/components/custom/showResult";
 
 export const QuizPage = () => {
   const { quizData, loading, error } = useFetchQuiz();
